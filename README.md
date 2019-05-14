57f1683a1691f9b16429c0763bf2d832ed010566
CrsithianRonad <cris16cone@gmail.com>
2019-05-14 00:38:17 -0500

    string
    este cap es de string

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 Curso
+Se inicia este repo como guia de curso de fazt (Curso Pythonpara Principiantes)
+Recuperado de:
+www.youtube.com/watch?v=chPhlsHoEPo&t
--- string.py
+++ string.py
@@ -1,1 +1,28 @@
-print('hola')
+myStr = "hola mundo"
+name="Cristhian"
+print(myStr)
+print("Cris de vuelta " + myStr)
+print(f"My name is {name} jaja")
+print("My name is {0}".format(name))
+print(dir(myStr))
+print(myStr.upper())
+print(myStr.lower())
+print(myStr.capitalize())
+print(myStr.replace("hola","adios"))
+print(myStr.count("o"))
+print(myStr.count(" "))
+
+print(myStr.startswith("hola"))
+print(myStr.endswith("chao"))
+
+print(myStr.split())
+print(myStr.find("a"))
+
+print(len(myStr))
+print(myStr.index("h"))
+
+print(myStr.isalnum())
+print(myStr.isalpha())
+
+print(myStr[5])
+print(myStr[-2])
